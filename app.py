e4a181d36d0b609bd7ff191d08e250acd80b66b8
Robert Davies <rob.davies1991@gmail.com>
2020-01-20 19:58:15 +0000

    Imported Flask/Mongo libraries along with creating basic bootstrap template

--- app.py
+++ app.py
@@ -1,5 +1,25 @@
+import os
 from os import path
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
 
 # Import local environment settings - Local only
 if path.exists("env.py"):
     import env 
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = os.environ['MONGO_DBNAME']
+app.config["MONGO_URI"] = os.environ['MONGO_URI']
+
+mongo = PyMongo(app)
+
+@app.route('/')
+def home():
+    return render_template("homepage.html")
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+    port=os.environ.get('PORT'),
+    debug=True)
