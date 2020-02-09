c81c515952c682dd29a9e71daea5e90f2cdd1e72
Robert Davies <rob.davies1991@gmail.com>
2020-02-09 12:05:26 +0000

    cleaned up app.py according to pep8online.com

--- app.py
+++ app.py
@@ -38,6 +38,7 @@ s3.init_app(app)
 # General Pages
 ###############
 
+
 @app.route('/')
 def home():
     recipes = mongo.db.recipes.count()
@@ -45,10 +46,10 @@ def home():
     tools = mongo.db.tools.count()
 
     # Finds the most common used cuisine
-    favoriteCuisine = mongo.db.recipes.aggregate([
-    {"$group": {"_id": "$cuisine","value": {"$sum": 1}}},{"$sort": {"value": -1}},{"$limit": 1}])
+    favoriteCuisine = mongo.db.recipes.aggregate([{"$group": {"_id": "$cuisine", "value": {"$sum": 1}}}, {"$sort": {"value": -1}}, {"$limit": 1}])
 
     return render_template("homepage.html", recipecount=recipes, cuisinecount=cuisines, toolcount=tools, favcuisine=list(favoriteCuisine))
+
 
 @app.route('/manage')
 def manage():
@@ -58,20 +59,21 @@ def manage():
 # Search Pages
 ###############
 
-@app.route('/simpleSearch/', methods=['GET','POST'])
+
+@app.route('/simpleSearch/', methods=['GET', 'POST'])
 def simpleSearch():
     simpleSearch = request.form.get('seachterm')
-    recipes = list(mongo.db.recipes.find({'name':{'$regex':'.*' + simpleSearch + '.*'}}))
-    return render_template("search_simple.html", search_query=simpleSearch, recipes=recipes,imagePath=app.config['UPLOAD_FOLDER_RECIPE'], s3link=app.config['AWS_BUCKET_LINK'])
+    recipes = list(mongo.db.recipes.find({'name': {'$regex': '.*' + simpleSearch + '.*'}}))
+    return render_template("search_simple.html", search_query=simpleSearch, recipes=recipes, imagePath=app.config['UPLOAD_FOLDER_RECIPE'], s3link=app.config['AWS_BUCKET_LINK'])
 
-@app.route('/advancedSearch/', methods=['GET','POST'])
+
+@app.route('/advancedSearch/', methods=['GET', 'POST'])
 def advancedSearch():
     searchterm = request.form.get('searchterm').lower()
     searchfield = request.form.get('searchfield')
-    recipes = list(mongo.db.recipes.find({searchfield:{'$regex':'.*' + searchterm + '.*'}}))
-    return render_template("search_advanced.html", search_field=searchfield,search_query=searchterm, recipes=recipes,imagePath=app.config['UPLOAD_FOLDER_RECIPE'], s3link=app.config['AWS_BUCKET_LINK'])
+    recipes = list(mongo.db.recipes.find({searchfield: {'$regex': '.*' + searchterm + '.*'}}))
+    return render_template("search_advanced.html", search_field=searchfield, search_query=searchterm, recipes=recipes, imagePath=app.config['UPLOAD_FOLDER_RECIPE'], s3link=app.config['AWS_BUCKET_LINK'])
 
-if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-    port=os.environ.get('PORT'),
-    debug=True)
+if __name__ == '__main__': app.run(host=os.environ.get('IP'),
+                                   port=os.environ.get('PORT'),
+                                   debug=True)
